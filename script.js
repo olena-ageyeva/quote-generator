b5363f5426ea3663b3a35e747f4b2ce7d14eb259
unknown <eageeva@gmail.com>
2020-09-29 21:28:29 -0400

    custom cors proxy

--- script.js
+++ script.js
@@ -12,37 +12,53 @@ function loading() {
 }
 
 //Hide Loading
-function conplete() {
+function complete() {
   if (!loader.hidden) {
     quoteContainer.hidden = false;
     loader.hidden = true;
   }
 }
 
+//Show New Quote
+function newQuote(apiQuotes) {
+  //Pick a random quote from array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  const { author, text } = quote;
+
+  if (author === "") {
+    authorText.innerText = "unknown";
+  } else {
+    authorText.innerText = author;
+  }
+  if (text > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.innerText = text;
+
+  console.log(quote);
+}
+
 //Get Quote From API
 async function getQuote() {
   loading();
-  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-  const apiUrl =
-    "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+  //const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const proxyUrl = "https://sheltered-badlands-52292.herokuapp.com/";
+  // const apiUrl =
+  //   "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+  const apiUrl = "https://type.fit/api/quotes";
 
   try {
     const response = await fetch(proxyUrl + apiUrl);
-    const data = await response.json();
-    if (data.quoteAuthor === "") {
-      authorText.innerText = "unknown";
-    } else {
-      authorText.innerText = data.quoteAuthor;
-    }
-    if (data.quoteText.length > 120) {
-      quoteText.classList.add("long-quote");
-    } else {
-      quoteText.classList.remove("long-quote");
-    }
-    quoteText.innerText = data.quoteText;
+
+    const apiQuotes = await response.json();
+
+    newQuote(apiQuotes);
+
     complete();
   } catch (error) {
-    getQuote();
+    //getQuote();
   }
 }
 
